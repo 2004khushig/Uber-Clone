550e431128704bf3b0e5f72c0345cf18d3f0e8da
2004khushig <3004khushigupta@gmail.com>
2025-01-29 21:41:15 +0530

    user registration done

---
+++ Backend/models/user.models.js
@@ -0,0 +1,50 @@
+const mongoose=require('mongoose');
+const bcrypt=require('bcrypt');
+const jwt=require('jsonwebtoken');
+
+const UserSchema = mongoose.Schema({
+    fullname:{
+        firstname:{
+            type:String,
+            required:true,
+            minlength:[3,'First name must be atleast 3 characters long'],
+        
+        },
+        lastname:{
+            type:String,
+            minlength:[3,'Last name must be atleast 3 characters long'],
+        }
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        minlength:[5,'Email must be atleast 5 characters long'],
+
+    },
+    password:{
+        type:String,
+        required:true,
+        select:false,
+    },
+    socketID:{
+        type:String,
+    },
+
+})
+
+UserSchema.methods.generateAuthToken=function(){
+    const token=jwt.sign({_id:this._id},process.env.JWT_SECRET);
+    return token;
+}
+
+UserSchema.methods.comparePassword=async function(password){
+    return await bcrypt.compare(password,this.password);
+
+}
+UserSchema.statics.hashPassword=async function(password){
+    return await bcrypt.hash(password,10);
+}
+
+const userModel=mongoose.model('user',UserSchema);
+module.exports=userModel;
